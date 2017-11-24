11f290322227c6cb34cb7c0f0b675b01d8d67a18
bruce <liangming@teambition.com>
2017-11-24 11:13:01 +0800

    Solve 0004 problem

---
+++ ans_0004.py
@@ -0,0 +1,18 @@
+import re
+
+if __name__ == '__main__':
+	wordsCount = {}
+	f = open('0004.text', 'r')
+	for line in f:
+		line = line.replace('\n', '')
+		print(line)
+		# words = line.split(' ')
+		words = re.findall('[a-zA-Z0-9]+',line)
+
+		for word in words:
+			if word in wordsCount.keys():
+				wordsCount[word] += 1
+			else:
+				wordsCount[word] = 1
+	for key in wordsCount.keys():
+		print(key+":"+str(wordsCount[key]))
