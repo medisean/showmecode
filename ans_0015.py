9644241e082424f54e9acfbf50a2c4a0dbb1bc81
medisean <liangmingzou@163.com>
2017-11-29 22:51:59 +0800

    Solve 0015 problem

---
+++ ans_0015.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+#write city.txt to xls
+
+import json
+import xlwt
+
+def read(filename):
+    f = open(filename, 'r')
+    data = json.load(f)
+    return data
+
+def writeToXLS(lines):
+    book = xlwt.Workbook()
+    sheet = book.add_sheet("Sheet1")
+
+    for line in lines:
+        sheet.write(int(line)-1, 0, line)
+        sheet.write(int(line)-1, 1, lines[line])
+    book.save('0015/city.xls')
+
+if __name__ == '__main__':
+    data = read('0015/city.txt')
+    writeToXLS(data)
